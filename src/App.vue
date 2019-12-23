3a4a07a4d4c0d88e6e28f70da0ab2e8f7acb7642
zhousg <zhoushugang@itcast.cn>
2019-12-23 15:01:42 +0800

    使用element-ui&使用vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    APP COM
+    APP COM  <el-button type="success">成功按钮</el-button>
   </div>
 </template>
 
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+// 配置路由
+
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: []
+})
+
+export default router
