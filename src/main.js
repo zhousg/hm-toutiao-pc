ad31dd4260ef48b062f6a6ad3f90713f13bbb3d3
zhousg <zhoushugang@itcast.cn>
2019-12-23 11:56:48 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 日志的详细与否，生产环境  开发环境
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+// main.js 入口文件
+// 职责：做全局范围内的依赖和配置
+// 职责：创建一个根VUE实例
