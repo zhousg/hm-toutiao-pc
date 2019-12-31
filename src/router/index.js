a0943613781446d22ee7441d260d014d1946b299
zhousg <zhoushugang@itcast.cn>
2019-12-31 09:51:43 +0800

    基础布局&富文本基本使用

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Welcome from '@/views/welcome'
 import NotFound from '@/views/404'
 import Article from '@/views/article'
 import Image from '@/views/image'
+import Publish from '@/views/publish'
 
 Vue.use(VueRouter)
 
@@ -26,7 +27,9 @@ const router = new VueRouter({
         // 内容管理
         { path: '/article', component: Article },
         // 素材管理
-        { path: '/image', component: Image }
+        { path: '/image', component: Image },
+        // 发布与编辑文章
+        { path: '/publish', component: Publish }
       ]
     },
     // 通配以上规则没有的地址
---
+++ src/views/publish/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container-publish">
+    <el-card>
+      <div slot="header">
+        <my-bread>发布文章</my-bread>
+      </div>
+      <!-- 表单 -->
+      <el-form label-width="120px">
+        <el-form-item label="标题：">
+          <el-input v-model="articleForm.title" style="width:400px"></el-input>
+        </el-form-item>
+        <el-form-item label="内容：">
+            <quill-editor v-model="articleForm.content" :options="editorOption"></quill-editor>
+        </el-form-item>
+        <el-form-item label="封面：">
+          <el-radio-group v-model="articleForm.cover.type">
+            <el-radio :label="1">单图</el-radio>
+            <el-radio :label="3">三图</el-radio>
+            <el-radio :label="0">无图</el-radio>
+            <el-radio :label="-1">自动</el-radio>
+          </el-radio-group>
+          <div>封面组件</div>
+        </el-form-item>
+        <el-form-item label="频道：">
+          <my-channel v-model="articleForm.channel_id"></my-channel>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">发表</el-button>
+          <el-button>存入草稿</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+// 导入富文本相关资源
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import { quillEditor } from 'vue-quill-editor'
+export default {
+  components: {
+    quillEditor
+  },
+  data () {
+    return {
+      // 收集文章数据
+      articleForm: {
+        // 标题
+        title: null,
+        // 封面
+        cover: {
+          type: 1
+        },
+        // 频道ID
+        channel_id: null,
+        // 内容
+        content: null
+      },
+      // 富文本配置对象
+      editorOption: {}
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'></style>
