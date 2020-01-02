4f3c8e822ad1a69ea7d93d05edee385de3487b95
zhousg <zhoushugang@itcast.cn>
2020-01-02 11:22:28 +0800

    非父子传值

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
+    <!-- 测试 -->
+    <com-a></com-a>
+    <div>
+      <com-b></com-b>
+    </div>
     <!-- 一级路由组件显示位置 -->
-    <router-view></router-view>
+    <!-- <router-view></router-view> -->
   </div>
 </template>
 
 <script>
-export default { }
+import ComA from '@/test/com-a'
+import ComB from '@/test/com-b'
+export default {
+  components: { ComA, ComB }
+}
 </script>
 
 <style lang="less"></style>
---
+++ src/test/com-b.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='container'>B组件 <button @click="fn">B到A</button></div>
+</template>
+
+<script>
+import eventBus from '@/eventBus'
+export default {
+  methods: {
+    fn () {
+      eventBus.$emit('b2a', 'B组件的数据')
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'></style>
